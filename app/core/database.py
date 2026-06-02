a4fe14b39a124fead77355ea6d97b8a7caf1b6bc
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2026-06-02 12:28:20 -0400

    Add project structure and create database engine

---
+++ app/core/database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
+
+
+# Create the SQLAlchemy engine
+def get_engine():
+    return create_engine(
+        settings.DATABSE_URL,
+        connect_args={"check_same_thread": False}
+    )
+
+
+# Create SQLAlchemy session factory
+def get_session_factory(engine):
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine)
+
+    return SessionLocal
+
+
+# Create Tables or runs migrations
+def init_db():
+    engine = get_engine()
+    return engine, get_session_factory(engine)
+
+
+# FastAPI dependency to get DB session
+def get_db():
+    engine, SessionLocal = init_db()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Base class for SQLAlchemy models
+Base = declarative_base()
