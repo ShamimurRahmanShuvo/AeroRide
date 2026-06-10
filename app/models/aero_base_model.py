004bf3453b0b4a94ca7952d9f5eb866b4ea492bb
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2026-06-10 09:49:53 -0400

    Add base model

---
+++ app/models/aero_base_model.py
@@ -0,0 +1,31 @@
+import uuid
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class AeroBaseModel(DeclarativeBase):
+    pass
+
+
+class UUIDMixin:
+    id: Mapped[str] = mapped_column(
+        default=lambda: str(uuid.uuid4()),
+        primary_key=True,
+        unique=True,
+        nullable=False
+    )
+
+
+class TimestampMixin:
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    )
